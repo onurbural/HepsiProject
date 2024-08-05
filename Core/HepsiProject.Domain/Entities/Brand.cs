0ca65255a13eb3eaeca431a8dc4e0e402b02f4ba
onurbural <vwpassat43@outlook.com>
2024-08-05 17:05:19 +0300

    Entities added

---
+++ Core/HepsiProject.Domain/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using HepsiProject.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiProject.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Core/HepsiProject.Domain/Entities/Category.cs
@@ -0,0 +1,32 @@
+﻿using HepsiProject.Domain.Common;
+using HepsiProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiProject.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+            
+        }
+
+        public Category(int parentId, string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty{ get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
